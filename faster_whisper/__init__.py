8a642d3469f3874314517689f2a36bc4bed63c95
SecondFatBudgie <139413693+SecondFatBudgie@users.noreply.github.com>
2023-08-04 21:25:26 +0800

    __init__.py

--- faster_whisper/__init__.py
+++ faster_whisper/__init__.py
@@ -3,10 +3,11 @@ from faster_whisper.transcribe import WhisperModel
 from faster_whisper.utils import download_model, format_timestamp
 from faster_whisper.version import __version__
 
+# Public interface of the faster_whisper package
 __all__ = [
-    "decode_audio",
-    "WhisperModel",
-    "download_model",
-    "format_timestamp",
-    "__version__",
+    "decode_audio",  # Module for audio decoding
+    "WhisperModel",  # Module for the WhisperModel class
+    "download_model",  # Function for downloading models
+    "format_timestamp",  # Function for formatting timestamps
+    "__version__",  # Package version
 ]
